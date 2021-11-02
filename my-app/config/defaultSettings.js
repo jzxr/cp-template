86b07b6f703d86860b6e20fd2141f9e501f948d9
jzxr <jasminezxr@gmail.com>
2021-11-03 00:24:58 +0800

    test ant design pro

---
+++ my-app/config/defaultSettings.js
@@ -0,0 +1,14 @@
+const Settings = {
+  navTheme: 'light',
+  // 拂晓蓝
+  primaryColor: '#1890ff',
+  layout: 'mix',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  pwa: false,
+  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  iconfontUrl: '',
+};
+export default Settings;
